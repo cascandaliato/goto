889b577a2e2e4f20aa93babfda70a22f920ada4d
casca <8927157+casca@users.noreply.github.com>
2020-07-18 22:42:14 +0200

    fix: cors (2)

--- api/shorten.go
+++ api/shorten.go
@@ -44,7 +44,7 @@ func init() {
 
 // ShortenHandler creates a shortURL from a given targetURL.
 func ShortenHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "casca.dev")
+	w.Header().Set("Access-Control-Allow-Origin", "https://casca.dev")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	if r.Method == "OPTIONS" {
