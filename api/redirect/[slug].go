5c697d40bd753c2bf2b0e9bb153b725463a1c179
casca <8927157+casca@users.noreply.github.com>
2020-07-19 08:46:14 +0200

    endpoint to redirect Fixes #6

---
+++ api/redirect/[slug].go
@@ -0,0 +1,73 @@
+package redirect
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type urlRedirect struct {
+	Slug      string `bson:"slug"`
+	TargetURL string `bson:"targetURL"`
+}
+
+// Handler ...
+func Handler(w http.ResponseWriter, r *http.Request) {
+	origin := "https://casca.dev"
+	if r.Header.Get("Origin") == "https://carmeloscandaliato.com" {
+		origin = "https://carmeloscandaliato.com"
+	}
+	w.Header().Set("Access-Control-Allow-Origin", origin)
+	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	targetURL, err := handlerWithError(w, r)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		http.Redirect(w, r, "/goto", 307)
+		return
+	}
+	http.Redirect(w, r, targetURL, 307)
+}
+
+func handlerWithError(w http.ResponseWriter, r *http.Request) (string, error) {
+	slug := r.URL.Query().Get("slug")
+	if slug == "" {
+		return "", e("could not extract slug from request", nil)
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB")))
+	if err != nil {
+		return "", e("error while connecting to database", err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	collection := client.Database("goto").Collection("redirects")
+
+	var redir urlRedirect
+	err = collection.FindOne(context.TODO(), bson.M{"slug": slug}).Decode(&redir)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return "", e("not found", err)
+		}
+		return "", e("error while looking for existing redirect", err)
+	}
+
+	return redir.TargetURL, nil
+}
+
+func e(message string, err error) error {
+	if err == nil {
+		return fmt.Errorf("%s", message)
+	}
+	return fmt.Errorf("%s: %v", message, err)
+}
