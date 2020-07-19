b260500354c60697b335da76484c145e697d6dfc
casca <8927157+casca@users.noreply.github.com>
2020-07-19 19:42:52 +0200

    "style: style app with tailwindcss"

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <hr />
-    <button @click="api">call api</button>
-    <br />
-    {{ data }}
-    <hr />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div
+      class="bg-gray-200 w-screen h-56 font-sans antialiased flex items-center justify-center text-gray-800 text-xl"
+    >
+      <div class="shadow-lg flex items-center justify-center max-w-3xl w-4/5 rounded-l-md">
+        <input
+          type="text"
+          class="placeholder-gray-500 p-2 rounded-l-md w-full focus:outline-none border-2 border-r-0 border-solid border-transparent focus:border-blue-500 tracking-wide"
+          placeholder="https://vuejs.org/v2/guide/forms.html"
+          v-model="targetURL"
+        />
+        <button
+          class="bg-blue-500 text-white p-2 rounded-r-md focus:outline-none lowercase border-2 border-solid border-transparent focus:bg-green-500 tracking-wide"
+          @click="api"
+        >shorten</button>
+      </div>
+    </div>
+    {{ targetURL }} -> {{ shortURL }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  },
-  data: () => ({ data: {} }),
+  components: {},
+  data: () => ({ shortURL: "", targetURL: "" }),
   methods: {
     async api() {
       try {
@@ -27,27 +34,16 @@ export default {
           process.env.NODE_ENV === "production" ? "/goto" : ""
         }/api/shorten`;
         const { data } = await axios.post(url, {
-          targetURL: `https://${Math.round(Math.random() * 100000)}.com/`
+          targetURL: this.targetURL
         });
-        this.data = data;
+        this.shortURL = data.shortURL;
       } catch (e) {
         console.log(e);
       }
     }
-  },
-  created() {
-    // setInterval(this.api, 5000);
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
