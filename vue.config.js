4b8734c44c3a9ee84c646590d59c24cc077a083a
casca <8927157+casca@users.noreply.github.com>
2020-08-09 15:58:36 +0200

    feat: revert routing changes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/goto/" : "/",
   devServer: {
     host: "0.0.0.0",
     disableHostCheck: true
-  }
+  },
+  outputDir: "dist/goto"
 };
