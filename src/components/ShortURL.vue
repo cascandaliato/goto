2e3d38f921ee6d8affe1c352aee06410fae8fb06
casca <8927157+casca@users.noreply.github.com>
2020-12-08 14:45:58 +0100

    fix: align tooltip color to new tailwind blue

--- src/components/ShortURL.vue
+++ src/components/ShortURL.vue
@@ -89,7 +89,7 @@ export default {
 }
 
 .tooltip .tooltip-inner {
-  background: #4299e1;
+  background: #3b82f6;
   color: white;
   font-weight: 500;
   border-radius: 6px;
@@ -102,7 +102,7 @@ export default {
   border-style: solid;
   position: absolute;
   margin: 5px;
-  border-color: #4299e1;
+  border-color: #3b82f6;
   z-index: 1;
 }
 
