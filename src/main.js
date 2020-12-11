27c332ddd27af76f3521db7441711402d9514eba
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-11 18:13:14 +0100

    feat: add GitHub and Twitter links to footer

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VTooltip from "v-tooltip";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faClipboard } from "@fortawesome/free-regular-svg-icons";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import VTooltip from "v-tooltip";
+import Vue from "vue";
+import App from "./App.vue";
 import "./assets/styles/index.css";
 
 library.add(faClipboard);
 library.add(faTimes);
+library.add(faGithub);
+library.add(faTwitter);
 
 Vue.use(VTooltip);
 Vue.config.productionTip = false;
